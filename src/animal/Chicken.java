d69d2d55d3bf9113c7c6467534f7f2d34e704ad5
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-23 22:40:46 +0700

    Hoàn thành bài 1

---
+++ src/animal/Chicken.java
@@ -0,0 +1,7 @@
+package animal;
+public class Chicken extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Chicken: cluck-cluck!");
+    }
+}
---
+++ src/animal/Tiger.java
@@ -0,0 +1,6 @@
+package animal;
+  public class Tiger extends Animal {
+    public void makeSound() {
+        System.out.println( "Tiger: roarrrrr!");
+    }
+}
